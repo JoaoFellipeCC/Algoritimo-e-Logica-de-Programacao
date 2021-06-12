5ca453169de9a69651f8453778c8bc3504d52e63
Joao-Fellipe <jfellipe1789@gmail.com>
2021-06-12 16:54:38 -0300

    Inserção da questão2
    Inseri a segunda questão, juntamente com o código necessário para concluir o que foi pedido, além de alterar o README para inserir a acentuação correta.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Algoritimo-e-Logica-de-Programacao
+# Algoritimo-e-Logica-de-Programação
  Repositorio onde serão inseridos assuntos e exercicios da materia de Algoritimo e Logica de Programação.
---
+++ bloco2/exercicios/pratica01/questão2/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+    float num, soma_par, soma_geral;
+    int resp, cont, teste, par, impar;
+    soma_geral=0;
+    cont=0;
+    par=0;
+    impar=0;
+    do
+    {
+        system("cls");
+        printf("Insira um numero inteiro qualquer: ");
+        scanf("%f", &num);
+        if (num>=0)
+        {
+            soma_geral+=num;
+            cont++;
+            if ((int)(num*10.0)%10==0)
+            {
+                teste=num;    
+                if (teste % 2 ==0)
+                {
+                    par++;
+                    soma_par+=num;
+                }else{
+                    impar++;
+                }
+                 
+            }else{
+                printf("E um numero com casas decimais, portanto, nao ha paricidade.");
+            }
+            
+        }else{
+            printf("NUMERO INVALIDO DIGITADO");
+        }
+        printf("Quer continuar inserindo numeros(sim=1/nao=0)?");
+        scanf("%d",&resp);
+    } while (resp==1);
+    printf("\nA quantidade de numeros pares digitados foi: %d.\n\n", par);
+    printf("A quantidade de numeros impares digitados foi: %d.\n\n", impar);
+    printf("A media geral dos numeros digitados foi: %.2f.\n\n", soma_geral/cont);
+}
