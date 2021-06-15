db851431807b0fd4ab0865390c7b41bf78b18be2
Joao-Fellipe <jfellipe1789@gmail.com>
2021-06-15 14:32:29 -0300

    Resolução de erros da questão2
    Algoritmo da questão 2 embelezada, além da resolução do erro da mostra de dados errados caso um número negativo fosse adicionado em meio á outros positivos.

--- bloco2/exercicios/pratica01/questão2/ex02.c
+++ bloco2/exercicios/pratica01/questão2/ex02.c
@@ -3,18 +3,21 @@
 
 main(){
     float num, soma_par, soma_geral;
-    int resp, cont, teste, par, impar;
+    int resp, cont, teste, par, impar, num_positivo;
     soma_geral=0;
     cont=0;
     par=0;
     impar=0;
+    num_positivo=0;
     do
     {
         system("cls");
-        printf("Insira um numero positivo qualquer: ");
+        printf("- - - - Dados dos numeros - - - -\n");
+        printf("\nInsira um numero positivo qualquer: ");
         scanf("%f", &num);
         if (num>=0)
         {
+            num_positivo++;
             soma_geral+=num;
             cont++;
             if ((int)(num*10.0)%10==0)
@@ -33,13 +36,23 @@ main(){
             }
             
         }else{
-            printf("NUMERO INVALIDO DIGITADO");
+            printf("\nNUMERO INVALIDO DIGITADO\n");
         }
-        printf("Quer continuar inserindo numeros(sim=1/nao=0)?");
+        printf("\nQuer continuar inserindo numeros(sim=1/nao=0)?");
         scanf("%d",&resp);
     } while (resp==1);
-    printf("\nA quantidade de numeros pares digitados foi: %d.\n\n", par);
-    printf("A quantidade de numeros impares digitados foi: %d.\n\n", impar);
-    printf("A media geral dos numeros digitados foi: %.2f.\n\n", soma_geral/cont);
+    system("cls");
+    if (num_positivo>0)
+    {
+        printf("\nA quantidade de numeros pares digitados foi: %d.\n", par);
+        printf("-------------------------------------------------------\n");
+        printf("A quantidade de numeros impares digitados foi: %d.\n", impar);
+        printf("-------------------------------------------------------\n");
+        printf("A media geral dos numeros digitados foi: %.2f.\n", soma_geral/cont);
+        printf("-------------------------------------------------------\n");
+    }else{
+        printf("\nAtencao: nao foram digitados valores validos para obtencao dos dados.\n");
+    }
+    printf("\n- - - - - - - -   Fim   - - - - - - - -\n");
     system("pause");
 }
